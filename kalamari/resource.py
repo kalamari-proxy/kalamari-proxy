277e8d0469cdcdbf4f28865e2f75c082a8989c35
Mark Hughes <Mark@Marks-MacBook-Pro-3.local>
2017-10-30 14:18:30 -0500

    Worked on converting json to strings for regex

--- kalamari/resource.py
+++ kalamari/resource.py
@@ -2,4 +2,47 @@ import json
 import urllib.request
 import sys
 
-print(lol this is python)
+blacklisturl = "https://kalamari-proxy.github.io/lists/blacklist.json"
+whitelisturl = "https://kalamari-proxy.github.io/lists/whitelist.json"
+cachelisturl = "https://kalamari-proxy.github.io/lists/cachelist.json"
+
+#function to obtain JSON object
+def obtainJSON(url):
+    req = urllib.request.urlopen(url)
+    data = req.read()
+
+#pretty sure encoding will always be utf-8
+#encoding = req.info().get_content_charset('utf-8')
+    data = json.loads(data.decode('utf-8'))
+    return data
+
+#function to convert domain path and misc into strings for regex
+def jsonTOstrings(jsonOBJ):
+    domains = ""
+    paths = ""
+    miscs = ""
+    strings = []
+    for d in jsonOBJ['domain']:
+        domains = domains + d + "|"
+    for p in jsonOBJ['path']:
+        paths = domains + p + "|"
+    for m in jsonOBJ['misc']:
+        miscs = domains + m + "|"
+    strings.append(domains)
+    strings.append(paths)
+    strings.append(miscs)
+    return strings
+
+# Generic list class
+class Resource_list():
+    def __init__(self,json):
+        self.json = obtainJSON(json)    # instance variable unique to each instance
+        #build regex here
+
+Blacklist = Resource_list(blacklisturl)
+
+#sanity check
+print(Blacklist.json['domain'])
+print("-------------------")
+s = jsonTOstrings(Blacklist.json)
+print(s)
