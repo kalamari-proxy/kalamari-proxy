9c5ce0d6e444bd9f5e32d5763d1ddc312968707a
Austin Hartzheim <inbox@austinhartzheim.me>
2017-10-31 17:17:22 +0000

    Fix binding so it works inside Docker.

--- kalamari/kalamari.py
+++ kalamari/kalamari.py
@@ -10,7 +10,7 @@ import config
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     proxy_instance = proxy.ProxyServer(loop)
-    coro = asyncio.start_server(proxy_instance.handler, '127.0.0.1', 3128,
+    coro = asyncio.start_server(proxy_instance.handler, '0.0.0.0', 3128,
                                 loop=loop)
     server = loop.run_until_complete(coro)
     try:
