6b30f1181dccd25bf0483de81a5d3f55f398fc21
Mark Hughes <mshughes@wisc.edu>
2017-11-01 11:46:31 -0500

    Changed regex , Fixed up format

--- kalamari/resource.py
+++ kalamari/resource.py
@@ -8,14 +8,20 @@ whitelisturl = "https://kalamari-proxy.github.io/lists/whitelist.json"
 cachelisturl = "https://kalamari-proxy.github.io/lists/cachelist.json"
 
 #function to obtain JSON object
-def obtainJSON(url):
+def obtain_json(url):
+    '''
+    function to obtain JSON object
+    '''
     req = urllib.request.urlopen(url)
     data = req.read()
     data = json.loads(data.decode('utf-8'))
     return data
 
-#function to convert domain path and misc into strings for regex
-def jsonTOstrings(jsonOBJ):
+
+def convert_json(jsonOBJ):
+    '''
+    function to convert domain path and misc into strings for regex
+    '''
     domains = ""
     paths = ""
     miscs = ""
@@ -37,22 +43,28 @@ def jsonTOstrings(jsonOBJ):
     strings.append(miscs)
     return strings
 
-# Generic list class
-class Resource_list:
+
+class ResourceList:
+    '''
+    Generic list class. Gets passed a url referring to Blacklist, Cachelist,
+    or Whitelist. Produces json object and REGEX.
+    '''
     def __init__(self,json):
-        self.json = obtainJSON(json)    # json object unique to each instance
-        #build regex here, will implement the jsonTOstrings function being printed below
-        # current regex is not correct
-        strings = jsonTOstrings(self.json)
-        self.regex = re.compile(r'http://' + r'(' + strings[0]+ r')' + r'('+strings[1]+r')' + strings[2] )
+        self.json = obtain_json(json)    # json object unique to each instance
+        strings = convert_json(self.json)
+        self.regex = re.compile(r'http://' + r'(' + strings[0]+ r')' + r'('+strings[1]+r')' + r'(.*)' )
 
-#will need to initialize these in their own function (refresh)
-Blacklist = Resource_list(blacklisturl)
-Whitelist = Resource_list(whitelisturl)
-Cachelist = Resource_list(cachelisturl)
 
-# Not how to set up these classes
-class Check_black:
+Blacklist = ResourceList(blacklisturl)
+Whitelist = ResourceList(whitelisturl)
+Cachelist = ResourceList(cachelisturl)
+
+
+class CheckBlack:
+    '''
+    Checks url request against Blacklist REGEX. Returns True if request matches
+    REGEX.
+    '''
     def __init__(self,request):
         self.request = request
         match = Blacklist.regex.match(request)
@@ -62,7 +74,11 @@ class Check_black:
             result = False
         print (result)
 
-class Check_white:
+class CheckWhite:
+    '''
+    Checks url request against Whitelist REGEX. Returns True if request matches
+    REGEX.
+    '''
     def __init__(self,request):
         self.request = request
         match = Whitelist.regex.match(request)
@@ -72,7 +88,11 @@ class Check_white:
             result = False
         return result
 
-class Check_cache:
+class CheckCache:
+    '''
+    Checks url request against Cachelist REGEX. Returns True if request matches
+    REGEX.
+    '''
     def __init__(self,request):
         self.request = request
         match = Cachelist.regex.match(request)
@@ -82,16 +102,10 @@ class Check_cache:
             result = False
         return result
 
-#sanity check
-#print(Blacklist.json['domain'])
-#print("-------------------")
-# s = jsonTOstrings(Blacklist.json)
-# print(s[0])
-# print("-------------------")
-# print(s[1])
-# print("-------------------")
-# print(s[2])
+
+
 print(Blacklist.regex)
 
-Check_black('http://adf.ly/ads/.facebook.com/somedir/something.js')
-Check_black('http://freakshare.com/advertisements/')
+CheckBlack('http://adf.ly/ads/.facebook.com/somedir/something.js')
+CheckBlack('http://freakshare.com/advertisements/blahblahblah')
+CheckBlack('http://False.com/advertisements/')
