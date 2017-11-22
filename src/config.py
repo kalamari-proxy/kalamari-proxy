016f43f7f465ed0fee5095eecde36e98f078f104
Austin Hartzheim <inbox@austinhartzheim.me>
2017-11-22 02:26:15 +0000

    Properly read config values from environment variables.

--- src/config.py
+++ src/config.py
@@ -1,8 +1,28 @@
-blacklist = 'https://kalamari-proxy.github.io/lists/blacklist.json'
-whitelist = 'https://kalamari-proxy.github.io/lists/whitelist.json'
-cachelist = 'https://kalamari-proxy.github.io/lists/cachelist.json'
-ip_acl = '192.168.1.0/24,127.0.0.0/8,172.17.0.1/32'
-list_refresh = 3600
-webserver_ip = '127.0.0.1'
-webserver_port = '8080'
-timeout = 150
+#import logging
+import os
+
+DEFAULT_REFRESH = 3600
+DEFAULT_TIMEOUT = 150
+
+blacklist = os.getenv('BLACKLIST',
+                      'https://kalamari-proxy.github.io/lists/blacklist.json')
+whitelist = os.getenv('WHITELIST',
+                      'https://kalamari-proxy.github.io/lists/whitelist.json')
+cachelist = os.getenv('CACHELIST',
+                      'https://kalamari-proxy.github.io/lists/cachelist.json')
+ip_acl = os.getenv('IP_ACL', '192.168.1.0/24,127.0.0.0/8,172.17.0.1/32')
+
+try:
+    list_refresh = int(os.getenv('REFRESH', DEFAULT_REFRESH))
+except ValueError:
+    logging.warn('Could not parse REFRESH environment variable an an integer.')
+    list_refresh = DEFAULT_REFRESH
+
+webserver_ip = os.getenv('WEBSERVER_IP', '127.0.0.1')
+webserver_port = os.getenv('WEBSERVER_PORT', '8080')
+
+try:
+    timeout = os.getenv('TIMEOUT', DEFAULT_TIMEOUT)
+except ValueError:
+    logging.warn('Could not parse TIMEOUT environment variable as an integer.')
+    timeout = DEFAULT_TIMEOUT
