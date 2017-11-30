15aa8f0dbc5c6db2030112d5b75cb7225fa6fe4c
freiermuth <freiermuth@wisc.edu>
2017-11-30 16:05:33 -0600

    added what I have so far

---
+++ src/create-cert.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+SCRIPTNAME=`basename $0`
+CACRT=./ca.crt
+CAKEY=./ca.key
+
+#############
+# Functions #
+############
+
+logmessage() {
+    echo $SCRIPTNAME: $1
+}
+
+usage() {
+    echo "Usage: $SCRIPTNAME -n session# -c common_name 
+            [-s subject_alternative_name]
+       -n session number to use for naming files while working
+       -c common name for certificate
+       -s subject alternative names in comma-separated format
+"
+}
+
+generateConfig() {
+    echo "[ req ]
+default_bits       = 2048
+distinguished_name = req_distinguished_name
+req_extensions     = req_ext
+[ req_distinguished_name ]
+countryName                 = US
+stateOrProvinceName         = Wisconsin
+localityName               = Madison
+organizationName           = Kalamari Proxy
+commonName                 = $COMMON_NAME" > $SESSION_NUMBER.csr
+#[ req_ext ]
+#subjectAltName = @alt_names
+#[alt_names]
+#DNS.1   = bestflare.com
+#DNS.2   = usefulread.com
+#DNS.3   = chandank.com" > $SESSION_NUMBER.csr
+
+}
+
+signCertificate() {
+    # check to see if csr exists
+    if [ ! -r $SESSION_NUMBER.csr ]; then
+        logmessage "Cannot read $SESSION_NUMBER.csr"
+        exit 1
+    fi
+}
+
+########
+# Main #
+########
+
+# see if key and cert exist and are readable
+if [ ! -r $CACRT ] || [ ! -r $CAKEY ]; then
+    logmessage "Either $CAKEY or $CACRT does not exist or is not readable"
+    exit 1
+fi
+
+# process flags
+while getopts ":n:c:s:" opt; do
+    case $opt in
+        n)
+            SESSION_NUMBER=$OPTARG
+            ;;
+        c)
+            COMMON_NAME=$OPTARG
+            ;;
+        s)
+            SUBJECT_ALTERNATIVE_NAMES=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+# make sure mandatory options set
+if [ ! -n "$SESSION_NUMBER" ]; then
+    echo Error: Must specify session number
+    usage
+    exit 1
+fi
+if [ ! -n "$COMMON_NAME" ]; then
+    echo Error: Must specify common name
+    usage
+    exit 1
+fi
+
+# generate CSR
+generateConfig
+
+# sign certificate
+signCertificate
+
+# cleanup
+cleanup
