bc22cf1a81c7706f95852f1a73cd1b600363c276
Amarah <sharif.amarah@gmail.com>
2017-12-03 16:16:53 -0600

    Update entrypoint.sh
    Added logic to check or call generation of root certificate

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+
+#Check if root certificate exist, otherwise  call generation script
+curr_dir = `dirname $0`
+file = "$curr_dir/rootCA.key"
+
+if test -s "$file"
+then
+    echo "Root Certificate Exists"
+else
+    echo "Generating Root Certificate..."
+    sh genrc
+fi
+
 nginx &
 python3 ./kalamari.py
