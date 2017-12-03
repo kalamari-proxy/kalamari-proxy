18a89337010302534ecc6a8b74ce7965ffde27c7
Joshua Freiermuth <freiermuth@wisc.edu>
2017-12-03 15:04:11 -0600

    removed session number, generates CSR with or without SANs and cleans up after itself

--- src/create-cert.sh
+++ src/create-cert.sh
@@ -15,9 +15,8 @@ logmessage() {
 
 # prints usage for help text
 usage() {
-    echo "Usage: $SCRIPTNAME -n session# -c common_name 
+    echo "Usage: $SCRIPTNAME -c common_name 
             [-s subject_alternative_name]
-       -n session number to use for naming files while working
        -c common name for certificate
        -s subject alternative names in comma-separated format
 "
@@ -25,33 +24,62 @@ usage() {
 
 # create the CSR and key file for signing
 generateConfig() {
-    echo "[ req ]
+    # only do SAN stuff if SAN's present
+    if [ -n "$SUBJECT_ALTERNATIVE_NAMES" ]; then
+        echo "[ req ]
 default_bits       = 2048
 distinguished_name = req_distinguished_name
 req_extensions     = req_ext
+prompt             = no
 [ req_distinguished_name ]
-countryName                 = US
-stateOrProvinceName         = Wisconsin
+countryName                = US
+stateOrProvinceName        = Wisconsin
 localityName               = Madison
 organizationName           = Kalamari Proxy
-commonName                 = $COMMON_NAME" > $SESSION_NUMBER.cfg
-#[ req_ext ]
-#subjectAltName = @alt_names
-#[alt_names]
-#DNS.1   = bestflare.com
-#DNS.2   = usefulread.com
-#DNS.3   = chandank.com" > $SESSION_NUMBER.cfg
+commonName                 = $COMMON_NAME
+[ req_ext ]
+subjectAltName = @alt_names
+[alt_names]" > $COMMON_NAME.cfg
 
-    # Run openssl to create csr and private key for signing
+        declare -i dnsnum
+        dnsnum=1
+        sans=${SUBJECT_ALTERNATIVE_NAMES//,/ }
+        IFS=' '
+        for san in $sans; do
+            echo "DNS.$dnsnum   = $san" >> $COMMON_NAME.cfg
+            dnsnum=$((dnsnum+1))
+        done
+        
+        # Run openssl to create csr and private key for signing
+        openssl req -config $COMMON_NAME.cfg -nodes -new -newkey rsa:2048 -out $COMMON_NAME.csr -keyout $COMMON_NAME.key > /dev/null 2>&1
+        RETVAL=$?
+        
+        # cleanup config file
+        rm $COMMON_NAME.cfgh
+    else 
+        # generate CSR non-interactively without SAN's
+        openssl req -nodes -new -newkey rsa:2048 -out $COMMON_NAME.csr -keyout $COMMON_NAME.key \
+        -subj "/C=US/ST=Wisconsin/L=Madison/O=Kalamari Proxy/CN=$COMMON_NAME" > /dev/null 2>&1
+        RETVAL=$?
+    fi
+    
+    # make sure last operation completed successfully
+    if [ $RETVAL -ne 0 ]; then
+        logmessage "Could not create csr and key for $COMMON_NAME"
+        exit 1
+    fi
 }
 
 # create certificate with existing key and csr
 signCertificate() {
     # check to see if csr or key does not exist
-    if [ ! -r $SESSION_NUMBER.csr ] || [ ! -r $SESSION_NUMBER.key ]; then
-        logmessage "Cannot read $SESSION_NUMBER.csr and or $SESSION_NUMBER.key"
+    if [ ! -r $COMMON_NAME.csr ] || [ ! -r $COMMON_NAME.key ]; then
+        logmessage "Cannot read $COMMON_NAME.csr and or $COMMON_NAME.key"
         exit 1
     fi
+
+    # sign csr - TODO: Doesn't work yet, no CA being generated
+    openssl x509 -req -in $COMMON_NAME.csr -CA $CACRT -CAkey $CAKEY -CAcreateserial -out $COMMON_NAME.crt -days 365 -sha256
 }
 
 ########
@@ -65,11 +93,8 @@ if [ ! -r $CACRT ] || [ ! -r $CAKEY ]; then
 fi
 
 # process flags
-while getopts ":n:c:s:" opt; do
+while getopts ":c:s:" opt; do
     case $opt in
-        n)
-            SESSION_NUMBER=$OPTARG
-            ;;
         c)
             COMMON_NAME=$OPTARG
             ;;
@@ -85,11 +110,6 @@ while getopts ":n:c:s:" opt; do
 done
 
 # make sure mandatory options set
-if [ ! -n "$SESSION_NUMBER" ]; then
-    echo Error: Must specify session number
-    usage
-    exit 1
-fi
 if [ ! -n "$COMMON_NAME" ]; then
     echo Error: Must specify common name
     usage
@@ -101,6 +121,3 @@ generateConfig
 
 # sign certificate
 signCertificate
-
-# cleanup
-cleanup
